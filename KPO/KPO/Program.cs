1a01862e9096fba532d46c18f77b872c748d633b
Polgár József <jpolgar999@gmail.com>
2019-03-06 12:51:05 +0100

    #2:Pontok adása, Program teljesen kész

--- KPO/KPO/Program.cs
+++ KPO/KPO/Program.cs
@@ -13,7 +13,7 @@ namespace KPO
             //Program: Kő,papír,olló játék programozása!
             //Írta: Polgár József, 2019.03.06
             //Megoldandó feladat: 1. Ciklikus program futás mindaddig, amíg a játékos szeretne játszani!
-
+            Console.WriteLine("*****Ez egy kő papír ollós játék!: ");
             Random r = new Random();
 
             string compChoice = "";
@@ -29,15 +29,50 @@ namespace KPO
                 switch (Console.ReadKey(true).KeyChar)
                 {
                     case 'k':
-                        playerChoice = "Kő";
+                        playerChoice = "Kő ";
                         break;
                     case 'p':
-                        playerChoice = "Papír";
+                        playerChoice = "Papír ";
                         break;
                     case 'o':
-                        playerChoice = "Olló";
+                        playerChoice = "Olló ";
                         break;
 
+
+                }
+                //4.Feladat: Számítógép választásásnak kérdése
+                switch (r.Next(0,3))
+                {
+                    case 0:
+                        compChoice = "Kő ";
+                        break;
+                    case 1:
+                        compChoice = "Papír ";
+                        break;
+                    case 2:
+                        compChoice = "Olló ";
+                        break;
+
+
+                }
+                if (
+                    (playerChoice == "Kő" && compChoice == "Papír") ||  
+                        (playerChoice == "Papír" && compChoice == "Olló") ||
+                         (playerChoice == "Olló" && compChoice == "Kő")
+                    )
+                {
+                    Console.WriteLine("Számítógép: " + compChoice + "Te: " + playerChoice);
+                    Console.WriteLine("Vesztettél! Állás: Szg: {0} Játékos: {1}", ++compScore, playerScore);
+                }
+                else if (playerChoice == compChoice)
+                {
+                    Console.WriteLine("Számítógép: " + compChoice + "Te: " + playerChoice);
+                    Console.WriteLine("Vesztettél! Állás: Szg: {0} Játékos: {1}", compScore, playerScore);
+                }
+                else
+                {
+                    Console.WriteLine("Számítógép: " + compChoice + "Te: " + playerChoice);
+                    Console.WriteLine("Vesztettél! Állás: Szg: {0} Játékos: {1}", compScore, ++playerScore);
 
                 }
 
