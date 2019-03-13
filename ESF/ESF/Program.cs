b33e1f73ad72ada3665fd1b1efc32a73b7ef0b4c
Polgár József <jpolgar999@gmail.com>
2019-03-13 12:04:49 +0100

    #4:Ciklusok

--- ESF/ESF/Program.cs
+++ ESF/ESF/Program.cs
@@ -10,6 +10,7 @@ namespace ESF
     {
         static void Main(string[] args)
         {
+            int v = 0;
             //A létrehozott új típussal inicializálunk egy objektumot
             Excercise ex = new Excercise("Polgár József");
 
@@ -21,6 +22,26 @@ namespace ESF
             ex.PrintName();
 
             Console.WriteLine(ex.getName());
+
+            ///Ciklusok 
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("CIklus:<változóhivaztkozás I.>" + i);
+                Console.WriteLine("CIklus:<változóhivaztkozás II.> {0}", 1);
+            }
+            v = 2;
+            while (v>0)
+            {
+                Console.WriteLine("while ciklus");
+                v--;
+            }
+            v = 2;
+            do
+            {
+                Console.WriteLine("do while ciklus");
+                v--;
+            } while (v>0);
 
             //Beolvas egy karaktert a konzolról
             Console.ReadKey();
