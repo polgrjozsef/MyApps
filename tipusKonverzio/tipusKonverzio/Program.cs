e54fedb64768ab63d6e7c7aa03fc2fcf3a1c7ac4
Polgár József <jpolgar999@gmail.com>
2019-04-10 11:06:43 +0200

    #6: elágazás és  IS,AS

---
+++ tipusKonverzio/tipusKonverzio/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipusKonverzio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10;
+
+            switch (x is int)
+            {
+                case true:
+                    Console.WriteLine("Az x típusa int");
+                    break;
+                case false:
+                    Console.WriteLine("Az x típusa nem int");
+                    break;
+
+            }
+
+            object a = "123";
+            object b = "Hello";
+            object c = 10;
+
+            string aa = a as string;
+            Console.WriteLine(aa == null ? "NULL" : aa); // 123 
+
+            string bb = b as string;
+            Console.WriteLine(bb == null ? "NULL" : bb); // Hello 
+
+            string cc = c as string;
+            Console.WriteLine(cc == null ? "NULL" : cc); // NULL 
+
+
+            Console.ReadKey();
+        }
+    }
+}
